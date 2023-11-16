20f7724763165b0cd3e3f36c9012aeab9c940920
park zihoon <bbakzi417@gmail.com>
2023-11-16 11:16:29 +0900

    Initial commit

---
+++ src/main/java/com/example/targetrecruiting/TargetRecruitingApplication.java
@@ -0,0 +1,13 @@
+package com.example.targetrecruiting;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TargetRecruitingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TargetRecruitingApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/targetrecruiting/TargetRecruitingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.targetrecruiting;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TargetRecruitingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
